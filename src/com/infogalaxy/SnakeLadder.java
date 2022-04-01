c4936c640f56c217fdc11662dd006aa8384ae857
prathamesh25ig <prathameshbachhav25@gmail.com>
2022-04-01 11:11:11 +0530

    ADDED: Snake And Ladder Simulation Game

---
+++ src/com/infogalaxy/SnakeLadder.java
@@ -0,0 +1,16 @@
+package com.infogalaxy;
+
+public class SnakeLadder {
+
+    //UC-1- Player Start Game With Position 0;
+    public void addPosition() {
+        int position = 0;
+        System.out.println("Player Position is" + position);
+    }
+    public static void main(String args[]){
+        SnakeLadder dice = new SnakeLadder();
+        dice.addPosition();
+    }
+
+}
+
