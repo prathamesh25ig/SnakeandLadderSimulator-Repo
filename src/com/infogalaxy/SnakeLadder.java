5b94f8b499fb92f83e0de4be262867667b000953
prathamesh25ig <prathameshbachhav25@gmail.com>
2022-04-01 11:49:45 +0530

    MODIFIED:Rolling Dice to Get Number Between 1-6

--- src/com/infogalaxy/SnakeLadder.java
+++ src/com/infogalaxy/SnakeLadder.java
@@ -1,5 +1,8 @@
 package com.infogalaxy;
 
+import java.util.Random;
+import static java.lang.Math.*;
+
 public class SnakeLadder {
 
     //UC-1- Player Start Game With Position 0;
@@ -7,9 +10,15 @@ public class SnakeLadder {
         int position = 0;
         System.out.println("Player Position is" + position);
     }
+    public void addDice(){
+        Random random = new Random();
+        int dice  = (int) ((Math.random()*(6-1))+1);
+        System.out.println("Dice Number" + dice);
+    }
     public static void main(String args[]){
         SnakeLadder dice = new SnakeLadder();
         dice.addPosition();
+        dice.addDice();
     }
 
 }
