8749875c014c04ef6a0986667f7a1b31ddac482d
prathamesh25ig <prathameshbachhav25@gmail.com>
2022-04-21 10:22:22 +0530

    MODIFIED: UC-7-Playing Game With Fiend

--- src/com/infogalaxy/SnakeLadder.java
+++ src/com/infogalaxy/SnakeLadder.java
@@ -2,77 +2,80 @@ package com.infogalaxy;
 
 import java.util.Random;
 
-import static java.lang.Math.*;
-
 public class SnakeLadder {
     //PLayer's Position
     int position = 0;
-    // No.of Dice roll
-    int countRoll = 0;
 
 
     //UC-1 Player's Game Start with Position 0:
 
-    public void showPosition() {
+    public void showPosition(int playerNo) {
 
-        System.out.println("Player Position is " + position);
+        System.out.println("Player" + playerNo + " Position :" + position);
     }
-    public void Diceroll() {
-        //UC-5 Ensuring Player get Exact Winning position 100 :
-        while (position != 100 ) {
-            countRoll++;
-            Random random = new Random();
-            //UC-2 Rolling Dice to Get Number between 1-6:
-            int diceNo = (int) ((Math.random() * (7 - 1)) + 1);
-            System.out.println("Dice Number:" + diceNo);
 
-            int play = (int)((Math.random() * (4-1)) + 1);
-            //Player's Option to Playe Game:
-            System.out.println(play);
-            //UC-3 Checking PLayer's option to PLay:
-            switch (play) {
-                case Constants.NO_PLAY:
-                    position = position + diceNo;
-                    System.out.println("Stay on Same Position!");
-                    break;
-                case Constants.LADDER:
-                    System.out.println("LADDER!!");
-                    position = position + diceNo;
-                    // In case Player go Above 100:
-                    if (position>100) {
-                        System.out.println("Player is Out of Board!!");
-                        // PLayer Stays in Same Previous Position
-                        position = position - diceNo;
-                    }
-                    break;
-                case Constants.SNAKE:
-                    System.out.println("SNAKE!!!");
+    public void Diceroll(int playerNo) {
+
+        Random random = new Random();
+        //UC-2 Rolling Dice to Get Number between 1-6:
+        int diceNo = (int) ((Math.random() * (7 - 1)) + 1);
+        System.out.println("Dice Number:" + diceNo);
+
+        int play = (int) ((Math.random() * (4 - 1)) + 1);
+        //Player's Option to Playe Game:
+        System.out.println(play);
+        //UC-3 Checking PLayer's option to PLay:
+        switch (play) {
+            case Constants.NO_PLAY:
+                position = position + diceNo;
+                System.out.println("Stay on Same Position!");
+                break;
+            case Constants.LADDER:
+                System.out.println("LADDER!!");
+                position = position + diceNo;
+                // In case Player go Above 100:
+                if (position > 100) {
+                    System.out.println("Player is Out of Board!!");
+                    // PLayer Stays in Same Previous Position
                     position = position - diceNo;
-                    // Ensuring player's position not get below 0:
-                    if(position<0){
-                        position = 0;
-                    }
-                    break;
-            }
-            // position after every Dice Roll
-            showPosition();
+                }
+                break;
+            case Constants.SNAKE:
+                System.out.println("SNAKE!!!");
+                position = position - diceNo;
+                // Ensuring player's position not get below 0:
+                if (position < 0) {
+                    position = 0;
+                }
+                break;
         }
+        showPosition(playerNo);
     }
-    //UC-6 reporting No.of dice Roll to Win:
-    public void printCountingDiceRoll() {
-        System.out.println("Number's of Dice Roll To Win Game!! "+countRoll);
-    }
-
 
     public static void main(String args[]) {
         //Creating Object:
         SnakeLadder Player_1 = new SnakeLadder();
-        //Calling Function through Object:
-        Player_1.showPosition();
-        Player_1.Diceroll();
-        Player_1.showPosition();
-        Player_1.printCountingDiceRoll();
+        SnakeLadder Player_2 = new SnakeLadder();
+
+        // Showing Player's Position
+        System.out.println("Game Start Position:");
+        Player_1.showPosition(1);
+        Player_2.showPosition(2);
+        // UC-7 Playing Game with 2 Players:
+        while (Player_1.position != 100 && Player_2.position != 100) {
+            System.out.println("Player 1 Chance:");
+            Player_1.Diceroll(1);
+            if (Player_1.position == 100){break;}
+            System.out.println("Player 2 Chance");
+            Player_2.Diceroll(2);
+        }
+        // Reporting Winner
+        if (Player_1.position == 100){
+            System.out.println("Player 1 is Winner!!!");
+        }
+        else {
+            System.out.println("Player 2 is Winner!!!!");
+        }
 
     }
-
 }
